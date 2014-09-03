3ce244250752655b8cf53fb012e04ac50f6bae30
Derek Schaefer <derek.schaefer@gmail.com>
2014-09-03 18:24:41 -0500

    Update logging

--- app.js
+++ app.js
@@ -3,12 +3,8 @@ var io = require('socket.io')(8010);
 io.on('connection', function(socket) {
   console.log('connection');
 
-  socket.on('pong', function() {
-    console.log('pong');
+  socket.on('ping', function() {
+    console.log('ping');
+    socket.emit('pong');
   });
-
-  setInterval(function() {
-    console.log('ping?');
-    socket.emit('ping');
-  }, 1000);
 });
